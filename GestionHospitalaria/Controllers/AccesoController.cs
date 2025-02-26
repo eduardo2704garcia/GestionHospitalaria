6af6fcc3d8c750c237138bd8c956a1ef19ff7fec
Eduardo David García Romero <eduagarcia985@gmail.com>
2025-02-25 21:59:17 -0500

    Comentar

--- GestionHospitalaria/Controllers/AccesoController.cs
+++ GestionHospitalaria/Controllers/AccesoController.cs
@@ -11,120 +11,120 @@ using System.Web;
 
 namespace CapaPresentacion.Controllers
 {
-    public class AccesoController : Controller
-    {
+    //public class AccesoController : Controller
+    //{
 
-        static string cadena = "Server=BALERION\\EDUARDO; Database=HospitalDB; Integrated Security=True; TrustServerCertificate=True;MultipleActiveResultSets=true";
-        public IActionResult Login()
-        {
-            return View();
-        }
+    //    static string cadena = "Server=BALERION\\EDUARDO; Database=HospitalDB; Integrated Security=True; TrustServerCertificate=True;MultipleActiveResultSets=true";
+    //    public IActionResult Login()
+    //    {
+    //        return View();
+    //    }
 
-        public IActionResult Registrar()
-        {
-            return View();
-        }
+    //    public IActionResult Registrar()
+    //    {
+    //        return View();
+    //    }
 
-        [HttpPost]
-        public ActionResult Registrar(UsuarioCLS oUsuario)
-        {
-            bool registrado;
-            string mensaje;
+    //    [HttpPost]
+    //    public ActionResult Registrar(UsuarioCLS oUsuario)
+    //    {
+    //        bool registrado;
+    //        string mensaje;
 
-            if (oUsuario.Clave == oUsuario.ConfirmarClave)
-            {
+    //        if (oUsuario.Clave == oUsuario.ConfirmarClave)
+    //        {
 
-                oUsuario.Clave = ConvertirSha256(oUsuario.Clave);
-            }
-            else
-            {
-                ViewData["Mensaje"] = "Las contraseñas no coinciden";
-                return View();
-            }
+    //            oUsuario.Clave = ConvertirSha256(oUsuario.Clave);
+    //        }
+    //        else
+    //        {
+    //            ViewData["Mensaje"] = "Las contraseñas no coinciden";
+    //            return View();
+    //        }
 
-            using (SqlConnection cn = new SqlConnection(cadena))
-            {
+    //        using (SqlConnection cn = new SqlConnection(cadena))
+    //        {
 
-                SqlCommand cmd = new SqlCommand("sp_RegistrarUsuario", cn);
-                cmd.Parameters.AddWithValue("Correo", oUsuario.Correo);
-                cmd.Parameters.AddWithValue("Clave", oUsuario.Clave);
-                cmd.Parameters.Add("Registrado", SqlDbType.Bit).Direction = ParameterDirection.Output;
-                cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 100).Direction = ParameterDirection.Output;
-                cmd.CommandType = CommandType.StoredProcedure;
+    //            SqlCommand cmd = new SqlCommand("sp_RegistrarUsuario", cn);
+    //            cmd.Parameters.AddWithValue("Correo", oUsuario.Correo);
+    //            cmd.Parameters.AddWithValue("Clave", oUsuario.Clave);
+    //            cmd.Parameters.Add("Registrado", SqlDbType.Bit).Direction = ParameterDirection.Output;
+    //            cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 100).Direction = ParameterDirection.Output;
+    //            cmd.CommandType = CommandType.StoredProcedure;
 
-                cn.Open();
+    //            cn.Open();
 
-                cmd.ExecuteNonQuery();
+    //            cmd.ExecuteNonQuery();
 
-                registrado = Convert.ToBoolean(cmd.Parameters["Registrado"].Value);
-                mensaje = cmd.Parameters["Mensaje"].Value.ToString();
-
-
-            }
-
-            ViewData["Mensaje"] = mensaje;
-
-            if (registrado)
-            {
-                return RedirectToAction("Login", "Acceso");
-            }
-            else
-            {
-                return View();
-            }
-        }
-
-        [HttpPost]
-        public ActionResult Login(UsuarioCLS oUsuario)
-        {
-            oUsuario.Clave = ConvertirSha256(oUsuario.Clave);
-
-            using (SqlConnection cn = new SqlConnection(cadena))
-            {
-
-                SqlCommand cmd = new SqlCommand("sp_ValidarUsuario", cn);
-                cmd.Parameters.AddWithValue("Correo", oUsuario.Correo);
-                cmd.Parameters.AddWithValue("Clave", oUsuario.Clave);
-                cmd.CommandType = CommandType.StoredProcedure;
-
-                cn.Open();
-
-                oUsuario.IdUsuario = Convert.ToInt32(cmd.ExecuteScalar().ToString());
-
-            }
-
-            if (oUsuario.IdUsuario != 0)
-            {
-
-                Session["usuario"] = oUsuario;
-                return RedirectToAction("Index", "Home");
-            }
-            else
-            {
-                ViewData["Mensaje"] = "usuario no encontrado";
-                return View();
-            }
+    //            registrado = Convert.ToBoolean(cmd.Parameters["Registrado"].Value);
+    //            mensaje = cmd.Parameters["Mensaje"].Value.ToString();
 
 
-        }
+    //        }
+
+    //        ViewData["Mensaje"] = mensaje;
+
+    //        if (registrado)
+    //        {
+    //            return RedirectToAction("Login", "Acceso");
+    //        }
+    //        else
+    //        {
+    //            return View();
+    //        }
+    //    }
+
+    //    [HttpPost]
+    //    public ActionResult Login(UsuarioCLS oUsuario)
+    //    {
+    //        oUsuario.Clave = ConvertirSha256(oUsuario.Clave);
+
+    //        using (SqlConnection cn = new SqlConnection(cadena))
+    //        {
+
+    //            SqlCommand cmd = new SqlCommand("sp_ValidarUsuario", cn);
+    //            cmd.Parameters.AddWithValue("Correo", oUsuario.Correo);
+    //            cmd.Parameters.AddWithValue("Clave", oUsuario.Clave);
+    //            cmd.CommandType = CommandType.StoredProcedure;
+
+    //            cn.Open();
+
+    //            oUsuario.IdUsuario = Convert.ToInt32(cmd.ExecuteScalar().ToString());
+
+    //        }
+
+    //        if (oUsuario.IdUsuario != 0)
+    //        {
+
+    //            Session["usuario"] = oUsuario;
+    //            return RedirectToAction("Index", "Home");
+    //        }
+    //        else
+    //        {
+    //            ViewData["Mensaje"] = "usuario no encontrado";
+    //            return View();
+    //        }
 
 
-        public static string ConvertirSha256(string texto)
-        {
-            //using System.Text;
-            //USAR LA REFERENCIA DE "System.Security.Cryptography"
+    //    }
 
-            StringBuilder Sb = new StringBuilder();
-            using (SHA256 hash = SHA256Managed.Create())
-            {
-                Encoding enc = Encoding.UTF8;
-                byte[] result = hash.ComputeHash(enc.GetBytes(texto));
 
-                foreach (byte b in result)
-                    Sb.Append(b.ToString("x2"));
-            }
+    //    public static string ConvertirSha256(string texto)
+    //    {
+    //        //using System.Text;
+    //        //USAR LA REFERENCIA DE "System.Security.Cryptography"
 
-            return Sb.ToString();
-        }
-    }
+    //        StringBuilder Sb = new StringBuilder();
+    //        using (SHA256 hash = SHA256Managed.Create())
+    //        {
+    //            Encoding enc = Encoding.UTF8;
+    //            byte[] result = hash.ComputeHash(enc.GetBytes(texto));
+
+    //            foreach (byte b in result)
+    //                Sb.Append(b.ToString("x2"));
+    //        }
+
+    //        return Sb.ToString();
+    //    }
+    //}
 }

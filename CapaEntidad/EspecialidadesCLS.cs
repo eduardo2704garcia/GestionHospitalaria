532d2ad5fa8a4c67c4875d0f65a450617977e062
Eduardo David García Romero <eduagarcia985@gmail.com>
2025-03-01 08:29:29 -0500

    especialidades cls

--- CapaEntidad/EspecialidadesCLS.cs
+++ CapaEntidad/EspecialidadesCLS.cs
@@ -1,14 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CapaEntidad
 {
+    [Table("Especialidades")]
     public class EspecialidadesCLS
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
         public string Nombre { get; set; }
 
     }

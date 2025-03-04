0d2dba4e82692ff9c409e84c3e2568c671cd946f
Eduardo David García Romero <eduagarcia985@gmail.com>
2025-03-04 09:56:19 -0500

    Cambios

--- CapaEntidad/AccesoCLS.cs
+++ CapaEntidad/AccesoCLS.cs
@@ -6,10 +6,12 @@ using System.Threading.Tasks;
 
 namespace CapaEntidad
 {
-    public class UsuarioCLS
+    public class AccesoCLS
     {
-        public int IdUsuario { get; set; }
-        public string Correo {  get; set; }
+        public int Id { get; set; }
+        public string Nombre  { get; set; }
+        public string Apellido { get; set; }
+        public string Correo { get; set; }
         public string Clave { get; set; }
 
     }
